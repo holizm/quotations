3cbce3af9554eb43329ad66e74a735a21a8482d0
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:01 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='requestQuote'
+    name='quotations'
+    title='quotationsQuotations'
+    {...props}
+/>
